dbb3fd14874986322297ac379dd931d4e341720a
higa1140 <p_drive@csc.jp>
2019-03-11 22:39:51 +0900

    url external file

--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -1,35 +1,33 @@
 <template>
   <div>
-    
-      <b-card v-for="(item, index) in items" :key="index"
-        :img-src="item.image"
-        img-alt="Image"
-        img-left
-        class="mb-3"
+    <b-card v-for="(item, index) in items" :key="index"
+      :img-src="item.image"
+      img-alt="Image"
+      img-left
+      class="mb-3"
       >
-        <b-link :to="item.personUrl">
-          <h5>{{item.personName}}</h5>
-        </b-link>
+      <b-link :to="item.personUrl">
+        <h5>{{item.personName}}</h5>
+      </b-link>
 
-        <a :href="item.shopUrl" class="card-link">{{item.shopName}} ({{item.place}})</a>
-        <b-card-text>
-          <b-badge v-if="item.isToday" variant="success">Today</b-badge>
-          <div v-for="(value, key) in item.schedule" :key="key">
-            <div v-if='value != "-"'>
-            {{key}} : {{value}}
-            </div>
+      <a :href="item.shopUrl" class="card-link">{{item.shopName}} ({{item.place}})</a>
+      <b-card-text>
+        <b-badge v-if="item.isToday" variant="success">Today</b-badge>
+        <div v-for="(value, key) in item.schedule" :key="key">
+          <div v-if='value != "-"'>
+          {{key}} : {{value}}
           </div>
+        </div>
 
-        </b-card-text>
-      </b-card>
-    
+      </b-card-text>
+    </b-card>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
-interface Hoge {
+interface ResponseItem {
   schedule: {[key: string]: string};
   isToday: boolean;
 
@@ -37,65 +35,56 @@ interface Hoge {
 
 @Component
 export default class Schedule extends Vue {
-  private yobi: string[] = new Array("日","月","火","水","木","金","土");
-  public items: Hoge[] = [];
-  constructor(){
+  private yobi: string[] = new Array('日', '月', '火', '水', '木', '金', '土');
+  private items: ResponseItem[] = [];
+  constructor() {
     super();
     this.getSchedule();
   }
 
   private getSchedule() {
-    var url = 'https://us-central1-higapro-180014.cloudfunctions.net/higa-check';
-    url+='?url=https%3A%2F%2Fwww.cityheaven.net%2Ftokyo%2FA1305%2FA130505%2Fcosplay-chikan%2Fgirlid-20595871%2F%3Flo%3D1';
-    url+='&url=https%3A%2F%2Fwww.cityheaven.net%2Ftokyo%2FA1305%2FA130505%2Fcoach-watashi%2Fgirlid-20962052%2F%3Flo%3D1';
-    url+='&url=https%3A%2F%2Fwww.cityheaven.net%2Ftokyo%2FA1304%2FA130404%2Fseifuku-o%2Fgirlid-22809307%2F%3Flo%3D1';
-    url+='&url=https%3A%2F%2Fwww.cityheaven.net%2Ftokyo%2FA1303%2FA130301%2Fshibuya-snoop%2Fgirlid-22623293%2F%3Flo%3D1';
-    url+='&url=https%3A%2F%2Fwww.cityheaven.net%2Ftokyo%2FA1303%2FA130301%2Fcennendo%2Fgirlid-24191028%2F%3Flo%3D1';
-    url+='&url=https%3A%2F%2Fwww.cityheaven.net%2Ftokyo%2FA1305%2FA130505%2Ffinemotion%2Fgirlid-19033949%2F%3Flo%3D1';
-    url+='&url=https%3A%2F%2Fwww.cityheaven.net%2Ftokyo%2FA1305%2FA130506%2Fhypergrace%2Fgirlid-19452781%2F%3Flo%3D1';
-    url+='&url=https%3A%2F%2Fwww.cityheaven.net%2Ftokyo%2FA1304%2FA130401%2Fsjk-finemotion%2Fgirlid-22524787%2F';
-    
+    let apiUrl = 'https://us-central1-higapro-180014.cloudfunctions.net/higa-check';
+
+    const urlsJson: {urls: string[]} = require('../assets/url.json');
+    apiUrl += urlsJson.urls.reduce((prev: string, current: string) => { return prev + '&url=' + current; }, '?');
+
     this.items = [];
 
-    let cb = ( response: Hoge[]) => {
-      let now = new Date();
-      var today: string = (now.getMonth() + 1 ) + "/" + now.getDate() + "(" + this.yobi[now.getDay()] + ")";
-
-      response.forEach((value: Hoge)=>{
-        let todaySchedule: string = value.schedule[today];
-        if (todaySchedule.match(/^\d{1,2}:\d{2}/)) {
-          value["isToday"] = true;
-        }
-
-        var schedule: {[key: string]: string} = {};
-        Object.keys(value.schedule).forEach((key: string) => {
-          if (value.schedule[key].match(/^\d{1,2}:\d{2}/)) {
-            schedule[key] = value.schedule[key];
-          }
-        });
-        value.schedule = schedule;
-        this.items.push(value);
-      });
-
-    };
-    this.requestAjax(url, cb);
+    this.requestAjax(apiUrl, this.apiCallback);
   }
 
+  private apiCallback(response: ResponseItem[])  {
+    const now = new Date();
+    const today: string = (now.getMonth() + 1 ) + '/' + now.getDate() + '(' + this.yobi[now.getDay()] + ')';
 
+    response.forEach((value: ResponseItem) => {
+      const todaySchedule: string = value.schedule[today];
+      if (todaySchedule.match(/^\d{1,2}:\d{2}/)) {
+        value.isToday = true;
+      }
 
-  private requestAjax (endpoint: string, callback: Function) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function(){
-      if (this.readyState==4 && this.status==200) {
+      const schedule: {[key: string]: string} = {};
+      Object.keys(value.schedule).forEach((key: string) => {
+        if (value.schedule[key].match(/^\d{1,2}:\d{2}/)) {
+          schedule[key] = value.schedule[key];
+        }
+      });
+      value.schedule = schedule;
+      this.items.push(value);
+    });
+  }
+
+  private requestAjax(endpoint: string, callback: (response: any) => void) {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
         callback(this.response);
       }
     };
     xhr.responseType = 'json';
-    xhr.open('GET',endpoint,true);
+    xhr.open('GET', endpoint, true);
     xhr.send();
-  };
-
-  
+  }
 }
 </script>
 
